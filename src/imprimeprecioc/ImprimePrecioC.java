0900c6e584f6bf9c9e2faaad0ad718faa53d39ec
erickGP27 <125601903+erickGP27@users.noreply.github.com>
2023-03-10 00:16:31 -0600

    Add files via upload

---
+++ src/imprimeprecioc/ImprimePrecioC.java
@@ -0,0 +1,28 @@
+package imprimeprecioc;
+import java.util.Scanner;
+
+/**
+ *
+ * @author maryse
+ *
+ * Scanner 
+ * Este programa calcula e imprime la cantidad de una orden de compra.
+ *
+ */
+public class ImprimePrecioC {
+    Scanner stdIn = new Scanner(System.in);
+     static double precio = 0; // precio de compra del artículo
+     static double cant = 0; // número de artículos comprados
+        public void IMP(double precio, double cant){ 
+        
+    public static void main(String[] args) {
+        ImprimePrecioC IMP = new ImprimePrecioC();
+      IMP. IMP(precio,cant);
+
+        
+       
+        
+        
+    }
+
+}
